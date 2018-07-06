b0e88cbee4fd1a68b0801f19cfe2360ea20286cc
Jahoops <jwhooper@gmail.com>
2018-07-06 09:56:57 -0400

    up

--- js/index.js
+++ js/index.js
@@ -84,14 +84,14 @@ $(document).ready(function () {
     document.body.style.cursor = 'default';
   });
 
-  $("#logo").blast({
-    delimiter: "letter"
-  }).velocity("transition.fadeIn", {
-    display: null,
-    duration: 1500,
-    stagger: 100,
-    delay: 1000
-  });
+  // $("#logo").blast({
+  //   delimiter: "letter"
+  // }).velocity("transition.fadeIn", {
+  //   display: null,
+  //   duration: 1500,
+  //   stagger: 100,
+  //   delay: 1000
+  // });
 
   $('canvas.rough.button').each(function (idx, el) {
     var rc = rough.canvas(el);
@@ -139,5 +139,56 @@ $(document).ready(function () {
       });
     }
   );
+  function doLogo(){
+    const strideSize = 4.6; // dist to get point sample 
+    const stepSize = 1.5; // iteration gap in userspace unit
+    const maxDist = 100; // to skip strokes with looooooong length
+    const strokeWidth = 0.098590522;//sketch line width; 
+    const strokeColor = "rgba(64,0,0,0.9)";//sketch color
+    const fillColor = "rgba(0,64,0,0.1)";//sketch line(a <path>) fill color
+    const sigBgFill = "transparent";//the elSig clone bg color
+    const fps = 60;
+    const duration = 0;//sign within `duration`
 
+    // Main
+    const draw = SVG("svg1");
+    const grp = draw.group().attr("transform",elGrp.getAttribute("transform"));
+    const path = grp.path(elSig.getAttribute("d")).fill(sigBgFill);
+    const spf = Math.ceil(path.length()/stepSize/fps);//strokes per frame
+
+    let i = 0;
+    setTimeout(function f() {
+      let n = 0;
+      while (n++ <spf) {
+        const a = path.pointAt(i);
+        const b = path.pointAt(i+strideSize);
+        const c = path.pointAt(i+strideSize*2);
+        const d = path.pointAt(i+strideSize*3);
+        const e = path.pointAt(i+strideSize*4);
+
+        if (!isLonger(a,d,maxDist) || !isLonger(a,e,maxDist)) {
+          grp.path(`M${a.x} ${a.y} C${b.x} ${b.y} ${c.x} ${c.y} ${d.x} ${d.y} 
+      M${a.x-1} ${a.y+1} C${c.x} ${c.y} ${d.x} ${d.y} ${e.x} ${e.y}`)
+            .fill(fillColor)
+            .stroke({width:strokeWidth, color:strokeColor})
+        }
+
+        i += stepSize;
+      }
+      if (i < path.length()) {
+        setTimeout(f, 1000/fps);             
+      } else {
+        path.fill('blue');
+      }
+    }, 10);
+
+
+    function isLonger(a, b, x) {
+      return Math.sqrt(Math.pow(a.x - b.x, 2), Math.pow(a.y - b.y, 2)) > x;
+    } 
+  }
+
+  window.setTimeout(function(){
+    doLogo();
+  },1);
 });
